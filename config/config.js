35f87db342fb8411cab8a8074a88d199d7901c7d
塔洛 <32660382+taluoo@users.noreply.github.com>
2018-05-16 09:54:51 +0800

    add config: qq_weiyun_sync_mac

--- config/config.js
+++ config/config.js
@@ -67,7 +67,32 @@ module.exports = {
                 let src = await iframe.getProperty('src');
                 return await src.jsonValue();
             } catch (e) {
+                console.log(e);
                 throw new Error(`qq_weiyun_mac 下载链接提取失败`);
+            }
+        }
+    },
+    'qq_weiyun_sync_mac': {
+        url: 'https://www.weiyun.com/download.html',
+        des: 'QQ WeiYun Mac 同步助手',
+        platform: 'mac',
+        folder: 'qq_weiyun_sync',
+        getLinkFromPage: async function (page) {
+            try {
+                await page.setRequestInterception(true);// 后面的 click 会导致下载，所以拦截一下
+                page.on('request', interceptedRequest => {
+                    if (interceptedRequest.url().endsWith('.dmg'))
+                        interceptedRequest.abort();
+                    else
+                        interceptedRequest.continue();
+                });
+                await page.click('a[data-action=downloadSync]'); // 根据页面逻辑，点击连接之后，#downloadIframe 的 src 会改为下载链接
+                let iframe = await page.$('#downloadIframe');
+                let src = await iframe.getProperty('src');
+                return await src.jsonValue();
+            } catch (e) {
+                console.log(e);
+                throw new Error(`qq_weiyun_sync_mac 下载链接提取失败`);
             }
         }
     }
